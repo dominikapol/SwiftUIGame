7327eda9d3e951374c94200b02cf0fc8792e189c
Dominika <dominikapiganova@mail.ru>
2021-11-15 19:16:59 +0300

    Initial Commit

---
+++ SwiftUiLection/SwiftUiLectionApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUiLectionApp.swift
+//  SwiftUiLection
+//
+//  Created by Dominika Poleshyck on 15.11.21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUiLectionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
